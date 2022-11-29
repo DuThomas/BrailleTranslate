061fbad6623ff9d5127dcfd85eaf780bf3ae30f9
Thomas Du <thomas.du@edu.ece.fr>
2022-11-29 12:24:43 +0100

    refactor: spacing

--- env/balayage.py
+++ env/balayage.py
@@ -5,7 +5,8 @@ import brailleTable
 import writechar
 
 # read image 
-image = cv2.imread("testTristan.png")
+# image = cv2.imread("testTristan.png")
+image = np.zeros((200, 200 , 3), np.uint8)
 #i -> colonnes; j -> lignes ;
 # i = 1
 # j = 1
@@ -13,49 +14,43 @@ print(image.shape)
 h, w, _ = image.shape
 for lig in range(h):
     for col in range(w):
-        image[lig,col]=[0,0,0]
+        image[lig, col] = [0, 0, 0]
 
-color=[255,255,255]
-image[10,40]=color
-image[40,10]=color
+color = [255, 255, 255]
+image[10,40] = color
+image[40,10] = color
 
-# image[203,103]=color
-# image[100,29]=color
-# image[190,100]=color
-# image[0,0]=color
-xmin=w-1
-ymin=h-1
-xmax=0
-ymax=0
+xmin = w - 1
+ymin = h - 1
+xmax = 0
+ymax = 0
 pasx = 4
 pasy = 6
 
 for lig in range(h):
     for col in range(w):
-        # print(image[lig,col])
-        # print(image[lig,col,0])
-        if(image[lig,col,0]==255):
+        if(image[lig,col,0] == 255):
             if(lig > ymax):                
                 ymax = lig
-            if(col > xmax):               
-                xmax = col
             if(lig < ymin):               
                 ymin = lig
+            if(col > xmax):               
+                xmax = col
             if(col < xmin):              
                 xmin = col
-carac=[xmin,ymin,xmax,ymax]
+carac = [xmin, ymin, xmax, ymax]
 print(carac)
 # cv2.rectangle(image,(xmin-1,ymin-1),(xmax+1,ymax+1),(0,0,255),1)
-for l in range(ymin,ymax,pasy):
-    if l!=0:
-        cv2.line(image,(xmin,l),(xmax,l),(0,0,255),1)
+for l in range(ymin, ymax, pasy):
+    if l != 0:
+        cv2.line(image, (xmin, l), (xmax, l), (0, 0, 255), 1)
 
-for k in range(xmin,xmax,pasx):
-    if k!=0:
-        cv2.line(image,(k,ymin),(k,ymax),(0,0,255),1)
-color=[255,255,255]
-image[10,40]=color
-image[40,10]=color
+for k in range(xmin, xmax, pasx):
+    if k != 0:
+        cv2.line(image, (k, ymin), (k, ymax), (0, 0, 255), 1)
+color = [255, 255, 255]
+image[10, 40] = color
+image[40, 10] = color
 # image[203,103]=color
 # image[100,29]=color
 # image[190,100]=color
@@ -63,9 +58,17 @@ image[40,10]=color
 # image[203,103]=color
 # image[100,29]=color
 # image[190,100]=color
-writechar.write(image,(ymin,xmin),'v')
-writechar.write2(image,(ymin,xmin),ymax-ymin,xmax-xmin,'t')
-cv2.imshow('////', image)
+writechar.write2(image, (ymin, xmin), 12, 12)
+
+scale_percent = 1200 # percent of original size
+width = int(image.shape[1] * scale_percent / 100)
+height = int(image.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+# resize image
+resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
+cv2.imshow('////', resized)
 # add wait key. window waits until user presses a key
 cv2.waitKey(0)
 # and finally destroy/close all open windows
--- env/writechar.py
+++ env/writechar.py
@@ -1,57 +1,43 @@
 import cv2
 import brailleTable
-def write(img,tlcorner,char):
+def write(img, tlcorner, char):
     print(tlcorner)
-    color=[255,255,255]
+    color=[255, 255, 255]
     m = ""
     for cle,val in brailleTable.brailleTable.items():
         if (val == char):
-            m=cle
-    x=0
-    y=0
-    # for j in range(5):
-
-    #     if (j==3):
-    #         x+=2
-    #         y=0
-    #     if (m[j]=='1'):
-    #         img[tlcorner[0]+y][tlcorner[1]+x]=color
-    #     y+=2
-            
+            m = cle
+    x = 0
+    y = 0
     for i in range(len(m)):
-        if(m[i]=='1'):
-            img[tlcorner[0]+i%3 * 2,tlcorner[1] + i//3 * 2]=color
+        if(m[i] == '1'):
+            img[tlcorner[0]+ i%3 * 2 + 1, tlcorner[1] + i//3 * 2 + 1] = color
 
-    # img[tlcorner[]+2,tlcorner[1]]=color
-    # img[tlcorner[0]+2,tlcorner[1]]=color
-    # img[tlcorner[0]+2,tlcorner[1]+2]=color
-    # img[tlcorner[0],tlcorner[1]+2]=color
-    # img[tlcorner[0],tlcorner[1]+4]=color
+def write2(img, tlcorner, h, w):
+    for lignes in range(0, h, 6):
+        for colonnes in range(0, w, 4):
+            scale_percent = 1200 # percent of original size
+            width = int(img.shape[1] * scale_percent / 100)
+            height = int(img.shape[0] * scale_percent / 100)
+            dim = (width, height)
+            # resize image
+            resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+            cv2.imshow('////', resized)
+            # add wait key. window waits until user presses a key
+            key = cv2.waitKey(0)
+            # print(ord('a'), ord('z'))
+            write3(img, (tlcorner[0] + lignes, tlcorner[1] + colonnes), chr(key))
 
-def write2(img,tlcorner,height,width,char):
-    for lignes in range(0,height,6):
-        for colonnes in range(0,width,4):
-            write3(img,(tlcorner[0]+lignes,tlcorner[1]+colonnes))
-
-def write3(img,tlcorner):
+def write3(img, tlcorner, char):
     print(tlcorner)
-    color=[255,255,255]
+    color = [255, 255, 255]
     m = ""
-    char = input()
+    # char = input()
     for cle,val in brailleTable.brailleTable.items():
         if (val == char):
-            m=cle
-    x=0
-    y=0
-    # for j in range(5):
-
-    #     if (j==3):
-    #         x+=2
-    #         y=0
-    #     if (m[j]=='1'):
-    #         img[tlcorner[0]+y][tlcorner[1]+x]=color
-    #     y+=2
-            
+            m = cle
+    x = 0
+    y = 0
     for i in range(len(m)):
-        if(m[i]=='1'):
-            img[tlcorner[0]+i%3 * 2,tlcorner[1] + i//3 * 2]=color
+        if(m[i] == '1'):
+            img[tlcorner[0] + i%3 * 2 + 1, tlcorner[1] + i//3 * 2 + 1] = color
